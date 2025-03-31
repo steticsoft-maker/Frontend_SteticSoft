790a453d0f02edad9810d531157af06bb9780c1e
Velsty <mrgerito@gmail.com>
2025-03-31 15:23:06 -0500

    Procesoventa

---
+++ frontend/src/modules/Ventas/ProcesoVentas.jsx
@@ -0,0 +1,21 @@
+import NavbarAdmin from "../../components/NavbarAdmin";
+
+function ProcesoVentas() {
+  return (
+    <div>
+      <NavbarAdmin />
+      <div className="home-container">
+        <header className="home-header">
+          <h1>Bienvenido a Nuestra Plataforma</h1>
+          <p>
+            Explora herramientas increíbles y gestiona tus necesidades con
+            nuestro sistema.
+          </p>
+        </header>
+    
+      </div>
+    </div>
+  );
+}
+
+export default ProcesoVentas;
