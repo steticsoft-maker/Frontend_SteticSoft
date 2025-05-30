2c754655f4fd8c15fdc854143cd9bdac9727636a
Velsty <117530498+Velsty@users.noreply.github.com>
2025-05-30 15:56:30 -0500

    quitando export repetidos y arreglando el ajuste del tamaño del dashboard

--- frontend/src/shared/components/common/index.js
+++ frontend/src/shared/components/common/index.js
@@ -5,11 +5,7 @@ export { default as Table } from "./Table";
 export { default as FormField } from "./FormField";
 export { default as Spinner } from "./Spinner";
 export { default as ErrorMessage } from "./ErrorMessage";
-export { default as ConfirmModal } from "./ConfirmModal"; 
-export { default as ValidationModal } from "./ValidationModal"; 
 export { default as ConfirmModal } from "./ConfirmModal";
 export { default as ValidationModal } from "./ValidationModal";
 export { default as ItemSelectionModal } from "./ItemSelectionModal";
 export { default as PdfViewModal } from "./PdfViewModal";
-
-// ...otros componentes comunes que vayas creando
