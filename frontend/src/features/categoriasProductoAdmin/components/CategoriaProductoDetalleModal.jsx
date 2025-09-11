5a60e0545635654407d267beabdb9dcd5991d7e0
Eylin <llynin121@gmail.com>
2025-09-10 19:52:28 -0500

    cate

--- frontend/src/features/categoriasProductoAdmin/components/CategoriaProductoDetalleModal.jsx
+++ frontend/src/features/categoriasProductoAdmin/components/CategoriaProductoDetalleModal.jsx
@@ -11,13 +11,9 @@ const CategoriaProductoDetalleModal = ({ isOpen, onClose, categoria }) => {
         <div className="details-section">
           <p><strong>Nombre:</strong> {categoria.nombre}</p>
           <p><strong>Descripción:</strong> {categoria.descripcion}</p>
-          {/* CAMBIO: Usar categoria.vidaUtilDias */}
-          <p><strong>Vida Útil:</strong> {categoria.vidaUtilDias} días</p>
-          <p><strong>Tipo de Uso:</strong> {categoria.tipoUso}</p>
           <p><strong>Estado:</strong> {categoria.estado ? "Activa" : "Inactiva"}</p>
           <p>
             <strong>Productos Asociados:</strong>{" "}
-            {/* CAMBIO: Acceder a los nombres de los productos si existen */}
             {categoria.productos && categoria.productos.length > 0 ? (
               categoria.productos.map(producto => producto.nombre).join(", ")
             ) : (
